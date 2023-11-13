94ed350da1546e5a64f4b5b79dce1fb06fd30043
Yukai Huang <yukaihuangtw@gmail.com>
2023-11-13 17:46:07 +0800

    feat: config seem for webhook development

---
+++ scripts/seem.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+import SmeeClient from 'smee-client'
+
+dotenv.config()
+
+const smee = new SmeeClient({
+  source: process.env.WEBHOOK_PROXY_URL,
+  target: 'http://localhost:3000/events',
+  logger: console
+})
+
+smee.start()
