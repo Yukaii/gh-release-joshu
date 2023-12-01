dcfa865a5d511985f35c67baad849519ac1adca4
Yukai Huang <yukaihuangtw@gmail.com>
2023-12-01 22:24:37 +0800

    feat: basic model

---
+++ models/types.ts
@@ -0,0 +1,30 @@
+import { ColumnType, Selectable, Insertable } from 'kysely'
+
+export interface Database {
+  github_installation: GithubInstallationTable
+  github_installation_repository: GitHubInstallationRepositoryTable
+}
+
+export interface GithubInstallationTable {
+  // installation_id
+  id: ColumnType<number, number, never>
+  app_id: number
+  target_id: number
+  // User or Organization
+  target_type: 'User' | 'Organization'
+}
+
+export type GithubInstallation = Selectable<GithubInstallationTable>
+export type NewGithubInstallation = Insertable<GithubInstallationTable>
+
+export interface GitHubInstallationRepositoryTable {
+  id: ColumnType<number, number, never>
+  installation_id: number
+  repository_id: number
+  full_name: string
+  private: boolean
+}
+
+export type GitHubInstallationRepository = Selectable<GitHubInstallationRepositoryTable>
+export type NewGitHubInstallationRepository = Insertable<GitHubInstallationRepositoryTable>
+
