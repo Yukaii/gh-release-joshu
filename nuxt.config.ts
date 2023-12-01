72860f652c6525c07fa6dcd08e64e48068fcfaae
Yukai Huang <yukaihuangtw@gmail.com>
2023-12-01 22:24:13 +0800

    refactor: revert src

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,13 @@ export default defineNuxtConfig({
     githubAppPrivateKey: process.env.GITHUB_APP_PRIVATE_KEY,
     appId: process.env.GITHUB_APP_ID,
     developmentInstallationId: process.env.GITHUB_APP_DEVELOPMENT_INSTALLATION_ID,
-  },
-  rootDir: './src',
+    webhookSecret: process.env.GITHUB_APP_WEBHOOK_SECRET,
+    db: {
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      port: process.env.DB_PORT,
+      password: process.env.DB_PASSWORD,
+    }
+  }
 })
