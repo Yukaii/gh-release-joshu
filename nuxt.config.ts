b75b8d832dd85f7a14d87cdae57aa3d7cda4a233
Yukai Huang <yukaihuangtw@gmail.com>
2023-12-01 20:28:43 +0800

    refactor: move nuxt files under src

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,5 +6,6 @@ export default defineNuxtConfig({
     githubAppPrivateKey: process.env.GITHUB_APP_PRIVATE_KEY,
     appId: process.env.GITHUB_APP_ID,
     developmentInstallationId: process.env.GITHUB_APP_DEVELOPMENT_INSTALLATION_ID,
-  }
+  },
+  rootDir: './src',
 })
