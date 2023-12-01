6d2b9d5fc3421ca51e166253ad9742ba3bbed75b
Yukai Huang <yukaihuangtw@gmail.com>
2023-12-02 00:51:26 +0800

    feat: test out simple query

--- server/githubEventHandler.ts
+++ server/githubEventHandler.ts
@@ -1,6 +1,15 @@
+import { db } from "~/server/models";
+
 const eventHandlers: Record<string, (body: any) => Promise<void>> = {
   "installation.created": async (body) => {
     console.log("installation.created", body);
+
+    const records = await db
+      .selectFrom("github_installation")
+      .selectAll()
+      .execute();
+
+    console.log("records", records);
   },
 };
 
