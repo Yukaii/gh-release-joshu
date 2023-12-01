f71c0c592bc246f7afd6d3cd20e2244c94a026d8
Yukai Huang <yukaihuangtw@gmail.com>
2023-12-02 00:51:16 +0800

    refactor: extract dialects initialization

--- server/models/db.ts
+++ server/models/db.ts
@@ -1,18 +1,11 @@
-import { type Database } from "./types"; // this is the Database interface we defined earlier
-import { Pool } from "pg";
-import { Kysely, PostgresDialect } from "kysely";
+import { Kysely } from "kysely";
+
+import { type Database } from "./types";
+import { initDialect } from "~/utils/dbUtils";
 
 const config = useRuntimeConfig();
 
-const dialect = new PostgresDialect({
-  pool: new Pool({
-    database: config.db.database,
-    host: config.db.host,
-    user: config.db.user,
-    port: parseInt(config.db.port, 10),
-    max: 10,
-  }),
-});
+const dialect = initDialect(config.db, 10);
 
 export const db = new Kysely<Database>({
   dialect,
---
+++ utils/dbUtils.ts
@@ -0,0 +1,19 @@
+import pg from "pg";
+import { PostgresDialect } from "kysely";
+
+export function initDialect(dbConfig: {
+  database: string
+  host: string
+  user: string
+  port: string
+}, max: number | undefined): PostgresDialect {
+  return new PostgresDialect({
+    pool: new pg.Pool({
+      database: dbConfig.database,
+      host: dbConfig.host,
+      user: dbConfig.user,
+      port: parseInt(dbConfig.port, 10),
+      max,
+    }),
+  });
+}
