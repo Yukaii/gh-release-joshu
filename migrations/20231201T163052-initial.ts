4110ce2c362735f822ff89f622715fe4fd5b4d4e
Yukai Huang <yukaihuangtw@gmail.com>
2023-12-02 13:23:04 +0800

    feat: kysely adapter with auth.js

--- migrations/20231201T163052-initial.ts
+++ migrations/20231201T163052-initial.ts
@@ -1,4 +1,4 @@
-import { Kysely, sql } from "kysely";
+import { type Kysely, sql } from "kysely";
 
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
@@ -22,7 +22,9 @@ export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable("github_installation_repository")
     .addColumn("id", "integer", (col) => col.primaryKey())
-    .addColumn("installation_id", "integer", (col) => col.notNull().references("github_installation.id"))
+    .addColumn("installation_id", "integer", (col) =>
+      col.notNull().references("github_installation.id"),
+    )
     .addColumn("full_name", "text", (col) => col.notNull())
     .addColumn("private", "boolean", (col) => col.notNull())
     .execute();
@@ -39,12 +41,84 @@ export async function up(db: Kysely<any>): Promise<void> {
     .on("github_installation_repository")
     .column("full_name")
     .execute();
+
+  // Auth.js tables
+  await db.schema
+    .createTable("User")
+    .addColumn("id", "uuid", (col) =>
+      col.primaryKey().defaultTo(sql`gen_random_uuid()`),
+    )
+    .addColumn("name", "text")
+    .addColumn("email", "text", (col) => col.unique().notNull())
+    .addColumn("emailVerified", "timestamptz")
+    .addColumn("image", "text")
+    .execute();
+
+  await db.schema
+    .createTable("Account")
+    .addColumn("id", "uuid", (col) =>
+      col.primaryKey().defaultTo(sql`gen_random_uuid()`),
+    )
+    .addColumn("userId", "uuid", (col) =>
+      col.references("User.id").onDelete("cascade").notNull(),
+    )
+    .addColumn("type", "text", (col) => col.notNull())
+    .addColumn("provider", "text", (col) => col.notNull())
+    .addColumn("providerAccountId", "text", (col) => col.notNull())
+    .addColumn("refresh_token", "text")
+    .addColumn("access_token", "text")
+    .addColumn("expires_at", "bigint")
+    .addColumn("token_type", "text")
+    .addColumn("scope", "text")
+    .addColumn("id_token", "text")
+    .addColumn("session_state", "text")
+    .execute();
+
+  await db.schema
+    .createTable("Session")
+    .addColumn("id", "uuid", (col) =>
+      col.primaryKey().defaultTo(sql`gen_random_uuid()`),
+    )
+    .addColumn("userId", "uuid", (col) =>
+      col.references("User.id").onDelete("cascade").notNull(),
+    )
+    .addColumn("sessionToken", "text", (col) => col.notNull().unique())
+    .addColumn("expires", "timestamptz", (col) => col.notNull())
+    .execute();
+
+  await db.schema
+    .createTable("VerificationToken")
+    .addColumn("identifier", "text", (col) => col.notNull())
+    .addColumn("token", "text", (col) => col.notNull().unique())
+    .addColumn("expires", "timestamptz", (col) => col.notNull())
+    .execute();
+
+  await db.schema
+    .createIndex("Account_userId_index")
+    .on("Account")
+    .column("userId")
+    .execute();
+
+  await db.schema
+    .createIndex("Session_userId_index")
+    .on("Session")
+    .column("userId")
+    .execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await sql`ALTER TABLE github_installation_repository DROP CONSTRAINT github_installation_repository_pkey;`.execute(db);
+  await sql`ALTER TABLE github_installation_repository DROP CONSTRAINT github_installation_repository_pkey;`.execute(
+    db,
+  );
   await db.schema.dropTable("github_installation_repository").execute();
 
-  await sql`ALTER TABLE github_installation DROP CONSTRAINT github_installation_pkey;`.execute(db);
+  await sql`ALTER TABLE github_installation DROP CONSTRAINT github_installation_pkey;`.execute(
+    db,
+  );
   await db.schema.dropTable("github_installation").execute();
+
+  await db.schema.dropTable("Account").ifExists().execute();
+  await db.schema.dropTable("Session").ifExists().execute();
+  await db.schema.dropTable("User").ifExists().execute();
+  await db.schema.dropTable("VerificationToken").ifExists().execute();
 }
--- server/models/types.ts
+++ server/models/types.ts
@@ -1,29 +1,68 @@
-import { type ColumnType, type Selectable, type Insertable } from "kysely";
+import {
+  type ColumnType,
+  type Selectable,
+  type Insertable,
+  type GeneratedAlways,
+} from "kysely";
 
 export interface Database {
-  github_installation: GithubInstallationTable
-  github_installation_repository: GitHubInstallationRepositoryTable
+  github_installation: GithubInstallationTable;
+  github_installation_repository: GitHubInstallationRepositoryTable;
+
+  // Auth.js tables
+  User: {
+    id: GeneratedAlways<string>;
+    name: string | null;
+    email: string;
+    emailVerified: Date | null;
+    image: string | null;
+  };
+  Account: {
+    id: GeneratedAlways<string>;
+    userId: string;
+    type: string;
+    provider: string;
+    providerAccountId: string;
+    refresh_token: string | null;
+    access_token: string | null;
+    expires_at: number | null;
+    token_type: string | null;
+    scope: string | null;
+    id_token: string | null;
+    session_state: string | null;
+  };
+  Session: {
+    id: GeneratedAlways<string>;
+    userId: string;
+    sessionToken: string;
+    expires: Date;
+  };
+  VerificationToken: {
+    identifier: string;
+    token: string;
+    expires: Date;
+  };
 }
 
 export interface GithubInstallationTable {
   // installation_id
-  id: number
-  app_id: number
-  target_id: number
+  id: number;
+  app_id: number;
+  target_id: number;
   // User or Organization
-  target_type: "User" | "Organization"
+  target_type: "User" | "Organization";
 
-  created_at: ColumnType<Date, Date | undefined, never>
+  created_at: ColumnType<Date, Date | undefined, never>;
 }
 
 export type GithubInstallation = Selectable<GithubInstallationTable>;
 export type NewGithubInstallation = Insertable<GithubInstallationTable>;
 
 export interface GitHubInstallationRepositoryTable {
-  id: number
-  installation_id: number
-  full_name: string
-  private: boolean
+  id: number;
+  installation_id: number;
+  full_name: string;
+  private: boolean;
 }
 
 export type GitHubInstallationRepository =
