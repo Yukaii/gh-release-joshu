d56e915f87da14d5afb6ba0ea741a76c61eb8931
Yukai Huang <yukaihuangtw@gmail.com>
2023-12-02 12:03:26 +0800

    feat: setup nuxt-auth module

---
+++ server/api/auth/[...].ts
@@ -0,0 +1,14 @@
+import { NuxtAuthHandler } from "#auth";
+import GithubProvider from "next-auth/providers/github";
+
+const config = useRuntimeConfig();
+
+export default NuxtAuthHandler({
+  providers: [
+    // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
+    GithubProvider.default({
+      clientId: config.auth.github.clientId,
+      clientSecret: config.auth.github.clientSecret,
+    }),
+  ],
+});
