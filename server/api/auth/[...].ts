94ddf8db9234d2caedf39a649facd48680433e58
Yukai Huang <yukaihuangtw@gmail.com>
2023-12-02 13:23:04 +0800

    feat: kysely adapter with auth.js

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,9 +1,12 @@
 import { NuxtAuthHandler } from "#auth";
 import GithubProvider from "next-auth/providers/github";
+import { KyselyAdapter } from "@auth/kysely-adapter";
+import { db } from "~/server/models";
 
 const config = useRuntimeConfig();
 
 export default NuxtAuthHandler({
+  adapter: KyselyAdapter(db),
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GithubProvider.default({
--- server/models/db.ts
+++ server/models/db.ts
@@ -1,4 +1,4 @@
-import { Kysely } from "kysely";
+import { KyselyAuth } from "@auth/kysely-adapter";
 
 import { type Database } from "./types";
 import { initDialect } from "~/utils/dbUtils";
@@ -7,6 +7,6 @@ const config = useRuntimeConfig();
 
 const dialect = initDialect(config.db, 10);
 
-export const db = new Kysely<Database>({
+export const db = new KyselyAuth<Database>({
   dialect,
 });
